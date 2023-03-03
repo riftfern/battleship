d2f2860870c68001b410fe030540dc5d2f900067
riftfern <adriftfern@gmail.com>
2023-03-03 09:25:28 -0500

    added varibles

---
+++ src/components/viewables/base.js
@@ -0,0 +1,14 @@
+export const elements = {
+  p1Gameboard: document.querySelector(".p1-gameboard"),
+  p2Gameboard: document.querySelector(".p2-gameboard"),
+  p1Grid: document.querySelector(".p1-grid"),
+  p2Grid: document.querySelector(".p2-grid"),
+  infoContainer: document.querySelector(".info-container"),
+  infoText: document.querySelector(".info-text"),
+  autoPlaceBtn: document.querySelector(".fleet-autoplace"),
+  playAgainBtn: document.querySelector(".play-again"),
+  startBtn: document.querySelector(".start"),
+  fleetContainer: document.querySelector(".fleet-container"),
+  fleetDraggable: document.querySelector(".fleet-draggable"),
+  fleetInfo: document.querySelector(".fleet-info"),
+};
