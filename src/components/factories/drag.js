00ffe10a673ef26bd8877f572520cc0099392511
riftfern <adriftfern@gmail.com>
2023-03-03 09:51:52 -0500

    added functions

---
+++ src/components/factories/drag.js
@@ -0,0 +1,67 @@
+import { elements } from "../../views/base";
+import gameboardView from "../../views/gameboardView";
+
+const Drag = (p1, p1Board) => {
+  let draggedShip;
+  let draggedShipIndex;
+
+  //  Drag-N-Drop (for now for p1 only)
+  const getDraggedShipIndex = (e) =>
+    (draggedShipIndex = Number(e.target.dataset.index));
+
+  const dragStart = (e) => {
+    draggedShip = e.target;
+  };
+  const dragDrop = (e) => {
+    const cell = e.target;
+    const p1Ship = p1.getFleet()[draggedShip.dataset.ship];
+    const isHorizontal = p1Ship.getDirection() === "horizontal";
+    // get/adjust coords according to isHorizontal w/draggedShipIndex
+    const y = Number(cell.dataset.y) - (isHorizontal ? 0 : draggedShipIndex);
+    const x = Number(cell.dataset.x) - (isHorizontal ? draggedShipIndex : 0);
+
+    // place ship and get outcome
+    const outcome = p1Board.placeShip(p1Ship, y, x);
+    if (outcome) {
+      // update grid
+      gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
+      addDragAndDropEvenListeners();
+      // remove ship
+      draggedShip.parentElement.removeChild(draggedShip);
+      // show START button/ hide fleet-info if all ships are placed
+      if (p1Board.areAllShipsPlaced()) {
+        elements.startBtn.classList.add("show");
+        elements.fleetInfo.classList.add("hide");
+        elements.fleetInfo.classList.remove("show");
+      }
+    }
+  };
+
+  const dragOver = (e) => e.preventDefault();
+  const dragEnter = (e) => e.preventDefault();
+  const dragLeave = () => {};
+  const dragEnd = () => {};
+
+  const addDragAndDropEvenListeners = () => {
+    const ships = document.querySelectorAll(".ship");
+    const cells = elements.p1Grid.childNodes;
+
+    // Add EventListners for drag/drop events
+    for (const ship of ships) {
+      // EventListener to know which index is being held when dragging
+      ship.addEventListener("mousedown", getDraggedShipIndex);
+      ship.addEventListener("dragstart", dragStart);
+      ship.addEventListener("dragend", dragEnd);
+    }
+    for (const cell of cells) {
+      cell.addEventListener("dragover", dragOver);
+      cell.addEventListener("dragenter", dragEnter);
+      cell.addEventListener("dragleave", dragLeave);
+      cell.addEventListener("drop", dragDrop);
+    }
+  };
+
+  return { addDragAndDropEvenListeners };
+};
+
+export default Drag;
--- src/components/factories/game.js
+++ src/components/factories/game.js
@@ -1,5 +1,8 @@
 import Gameboard from "./gameboard";
 import Player from "./player";
+import Drag from "./drag";
+import gameboardView from "../viewables/gameboardView";
+import { elements } from "../viewables/base";
 
 const Game = (type) => {
   const p1 = Player("human");
@@ -9,7 +12,109 @@ const Game = (type) => {
   } else {
     p2 = Player("human");
   }
+  const p1Board = Gameboard();
+  const p2Board = Gameboard();
+
+  const drag = Drag(p1, p1Board);
+
+  const resetGame = () => {
+    p1.resetFleet();
+    p2.resetFleet();
+    p1Board.reset();
+    p2Board.reset();
+  };
+
+  const addRotateEventListeners = () => {
+    const ship = document.querySelectorAll(".ship");
+    ship.forEach((ship) => {
+      ship.addEventListener("dblclick", (e) => {
+        const shipElement = e.target.parentElement;
+        const ship = p1.getFleet()[shipElement.dataset.ship];
+        ship.changeDirection();
+        shipElement.classList.toggle("vertical");
+      });
+    });
+  };
+  const renderFleet = () => {
+    gameboardView.renderFleet(p1.getFleet());
+    drag.addDragAndDropEvenListeners();
+    addRotateEventListeners();
+  };
+
+  //  EventListener for p1 'human' player
+  const addGridEventListeners = () => {
+    if (p2.getType === "human")
+      elements.p1Grid.addEventListener("click", ctrlAttack);
+    elements.p2Grid.addEventListener("click", ctrlAttack);
+  };
+
+  // ctrlAttack function for eventListeners
+  const ctrlAttack = (e) => {
+    const cell = e.target;
+    if (cell.classList.contains("grid-cell")) {
+      // 1. Get coords from cell
+      const y = cell.dataset.y;
+      const x = cell.dataset.x;
+
+      // 2. Checks that board cell hasn't been attacked
+      const boardCell = p2Board.getBoard()[y][x];
+      if (boardCell !== "miss" && boardCell !== "hit") {
+        // 3. Makes Attacks for p1 'human' and p2 'computer'
+        p1.attack(y, x, p2Board);
+        p2.autoAttack(p1Board);
+
+        // 4. Updates grids after attacks to show outcome
+        renderGrids();
+      }
+      // 5. Checks if all ships are sunk
+      if (p1Board.areAllShipsSunk() || p2Board.areAllShipsSunk()) {
+        let winner = "";
+        if (p1Board.areAllShipsSunk()) {
+          winner = "Computer wins!";
+        } else if (p2Board.areAllShipsSunk()) {
+          winner = "You win!";
+        }
+        // 6. Disable eventListeners for attacks
+        elements.p2Grid.removeEventListener("click", ctrlAttack);
+        // 7. Display WiNNER / Play Again? Button'
+        gameboardView.renderWinner(winner);
+      }
+    }
+  };
+
+  // Render Grids / Update Grids
+  const renderGrids = () => {
+    gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
+    gameboardView.renderGrid(elements.p2Grid, p2Board, p2.getType());
+  };
+
+  const autoPlace = () => {
+    p1Board.reset();
+    p1Board.autoPlaceFleet(p1.getFleet());
+    renderGrids();
+    gameboardView.autoPlace();
+  };
+
+  const startGame = () => {
+    addGridEventListeners();
+    if (p2.getType() === "computer") p2Board.autoPlaceFleet(p2.getFleet());
+    gameboardView.startGame();
+  };
+
+  const playAgain = () => {
+    resetGame();
+    renderGrids();
+    gameboardView.playAgain();
+    renderFleet();
+  };
+
+  return {
+    renderGrids,
+    renderFleet,
+    autoPlace,
+    startGame,
+    playAgain,
+  };
 };
 
-const p1Board = Gameboard();
-const p2Board = Gameboard();
+export default Game;
