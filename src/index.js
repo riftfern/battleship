e02fb1721a974afa523df96d07590e74afb3a20a
riftfern <adriftfern@gmail.com>
2023-03-03 10:51:32 -0500

    added functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-export const sum = (a, b) =>  a + b ;
+import Game from "./components/factories/game";
+import { elements } from "./components/viewables/base";
+
+let gameType = "singleplayer";
+let game = Game(gameType);
+
+game.renderGrids();
+
+game.renderFleet();
+
+elements.autoPlaceBtn.addEventListener("click", (e) => {
+  console.log("autoplaced player fleet");
+  game.autoPlace();
+});
+
+elements.startBtn.addEventListener("click", (e) => {
+  console.log("GAME START");
+  game.startGame();
+});
+
+elements.playAgainBtn.addEventListener("click", (e) => {
+  console.log("LETS PLAY AGAIN");
+  game.playAgain();
+});
